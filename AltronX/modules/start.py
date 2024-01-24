4c0aee82c8522ce5c172fdcabdcd92666fcfa77c
Sachinsanatani4st <155264686+Sachinsanatani4st@users.noreply.github.com>
2024-01-25 02:05:05 +0530

    Update start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -5,14 +5,14 @@ import telethon
 
 PythonButton = [
         [
-        Button.inline("🍃𝐂σммαиԃ𝐒🌱", data="help_back")
+        Button.inline("ᴄσммαи∂ѕ", data="help_back")
         ],
         [
-        Button.url("✰𝐂ʜᴀᴛ_𝐆ʀᴘ☻︎", "https://t.me/II_4ST_FIGHTER_ll"),
-        Button.url("♕︎ 𝐒υρρσя𝐓 ♕︎", "https://t.me/I_M_FIGHTER")
+        Button.url("му ᴄнαт gʀρ︎", "https://t.me/II_4ST_FIGHTER_ll"),
+        Button.url("ѕυρρσʀт︎", "https://t.me/I_M_FIGHTER")
         ],
         [
-        Button.url("☞︎𝐑ᴇᴘᴏ☜︎", "https://t.me/ll4st_opII")
+        Button.url("ʀєρσ︎", "https://t.me/ll4st_opII")
         ]
         ]
 
@@ -33,12 +33,12 @@ async def start(event):
         AltBot = await event.client.get_me()
         BotName = AltBot.first_name
         BotId = AltBot.id
-        TEXT = f"**𝐇𝐞𝐲 [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝐈 𝐀𝐦 [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **✦ 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐃 𝐁𝐘 :~ [4sᴛ_𝐎ғғɪᴄɪᴀʟ](https://t.me/II_4ST_FIGHTER_ll)**\n\n"
-        TEXT += f"» **4sᴛ  𝐅𝐔𝐂𝐊𝐈𝐍𝐆 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 :** `3.2`\n"
-        TEXT += f"» **𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍:** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
+        TEXT = f"**нєℓℓσ му ∂єαʀ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝐈 𝐀𝐦 [{BotName}](tg://user?id={BotId})​**\n═━━━━━━━━━━━━━━━━━━━━━═\n\n"
+        TEXT += f"» **∂єνєℓσρє∂ ву :~ [ѕиαтαиι вσу](https://t.me/V_VIP_OWNER)**\n\n"
+        TEXT += f"» **𝗦𝝙𝗡𝝙𝗧𝝙𝗡𝗜 || 𝗦𝗣𝝙𝗠​ :** `3.2`\n"
+        TEXT += f"» **𝗧𝗘𝗟𝗘𝗧𝗛𝗜𝗡 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 :** `{telethon.__version__}`\n═━━━━━━━━━━━━━━━━━━━━━═"
         await event.client.send_file(
                 event.chat_id,
-                "https://telegra.ph/file/f5b0e7f2bd95153968e87.jpg",
+                "https://telegra.ph/file/204854c3a0cb8cfeae36c.jpg",
                 caption=TEXT, 
                 buttons=PythonButton)
