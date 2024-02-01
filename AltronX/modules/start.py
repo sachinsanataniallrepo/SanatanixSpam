929c5c3cc4c4408b0798528c20a1c5d22b6b57e3
sachinsanataniallrepo <158284520+sachinsanataniallrepo@users.noreply.github.com>
2024-02-01 18:12:45 +0530

    Update start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -5,14 +5,14 @@ import telethon
 
 PythonButton = [
         [
-        Button.inline("ᴄσммαи∂ѕ", data="help_back")
+        Button.inline("⌾ ᴄσммαи∂ѕ ⌾", data="help_back")
         ],
         [
-        Button.url("му ᴄнαт gʀρ︎", "https://t.me/II_4ST_FIGHTER_ll"),
-        Button.url("ѕυρρσʀт︎", "https://t.me/I_M_FIGHTER")
+        Button.url("⌾ му ᴄнαт gʀρ︎ ⌾", "https://t.me/II_4ST_FIGHTER_ll"),
+        Button.url("⌾ ѕυρρσʀт︎ ⌾", "https://t.me/I_M_FIGHTER")
         ],
         [
-        Button.url("ʀєρσ︎", "https://t.me/ll4st_opII")
+        Button.url("⌾ ʀєρσ︎ ⌾", "https://t.me/ll4st_opII")
         ]
         ]
 
@@ -33,12 +33,12 @@ async def start(event):
         AltBot = await event.client.get_me()
         BotName = AltBot.first_name
         BotId = AltBot.id
-        TEXT = f"**нєℓℓσ му ∂єαʀ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝐈 𝐀𝐦 [{BotName}](tg://user?id={BotId})​**\n═━━━━━━━━━━━━━━━━━━━━━═\n\n"
-        TEXT += f"» **∂єνєℓσρє∂ ву :~ [ѕиαтαиι вσу](https://t.me/V_VIP_OWNER)**\n\n"
-        TEXT += f"» **𝗦𝝙𝗡𝝙𝗧𝝙𝗡𝗜 || 𝗦𝗣𝝙𝗠​ :** `3.2`\n"
-        TEXT += f"» **𝗧𝗘𝗟𝗘𝗧𝗛𝗜𝗡 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 :** `{telethon.__version__}`\n═━━━━━━━━━━━━━━━━━━━━━═"
+        TEXT = f"**ㅤнєℓℓσ му ∂єαʀ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nㅤ⌾ 𝗜 𝗔𝗠 [{BotName}](tg://user?id={BotId})​**\n╭─────────────────────╮\n\n"
+        TEXT += f"» **ㅤ[ᯤ] ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ‣ [ѕαиαтαиι вσу](https://t.me/V_VIP_OWNER)**\n\n"
+        TEXT += f"» **ㅤ[ᯤ] 𝗦𝝙𝗡𝝙𝗧𝝙𝗡𝗜 || 𝗦𝗣𝝙𝗠 ‣** `3.2`\n"
+        TEXT += f"» **ㅤ[ᯤ] 𝗦𝝙𝗡𝝙𝗧𝝙𝗡𝗜 || 𝗦𝗣𝝙𝗠 ‣** `{telethon.__version__}`\n╰─────────────────────╯"
         await event.client.send_file(
                 event.chat_id,
                 "https://telegra.ph/file/204854c3a0cb8cfeae36c.jpg",
                 caption=TEXT, 
-                buttons=PythonButton)
+                buttons=PythonButton)￼Enter
